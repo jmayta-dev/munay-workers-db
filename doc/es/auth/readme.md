3731681324ad83e02cefd1565205de9d1f026f5b
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-10-27 23:58:56 -0500

    ✨(auth): initial setup

---
+++ doc/es/auth/readme.md
@@ -0,0 +1,104 @@
+# 💠 AUTH
+
+## TABLES
+<!-- <h3 id="users-table">🔸 USUARIOS (Users)</h3> -->
+### 🔸 USUARIOS (Users) {#users-table}
+
+![auth.Users](img/auth.Users.png)
+
+```sql
+CREATE TABLE [auth].[Users]
+(
+	  [Id]				[CHAR](24)			NOT NULL
+	, [Password]		[NVARCHAR](255)		NOT NULL
+	, [LastLogin]		[DATETIMEOFFSET]	NULL
+	, [IsSuperuser]		[BIT]				NULL
+	, [Username]		[VARCHAR](150)		NOT NULL
+	, [FirstName]		[NVARCHAR](250)		NULL
+	, [LastName]		[NVARCHAR](250)		NULL
+	, [Email]			[NVARCHAR](250)		NULL
+	, [IsStaff]			[BIT]				NULL
+	, [IsActive]		[BIT]				NOT NULL
+	, [DateJoined]		[DATETIMEOFFSET]	NULL
+	, [UpdatedAt]		[DATETIMEOFFSET]	NULL
+)
+```
+### Referencia:
++ 🔑 **Id**
+    <br> Identificador único conformado de un segmento numérico y uno alphanumérico.
+    <br>
+    <table>
+        <tr>
+            <td style="font-style:italic">estructura</td>
+            <td><code style="color:aquamarine">[0-9]{15}</code><code style="color:crimson">—</code><code style="color:aquamarine">[0-9a-zA-Z]{6}</code></td>
+        </tr>
+        <tr>
+            <td style="font-style:italic">ejemplo</td>
+            <td><code>189785402564147-x2f587</code></td>
+        </tr>
+    </table>
++ **Password**
+    <br> Contraseña encriptada con algoritmo `SHA`.
++ **LastLogin**
+    <br> Fecha y hora del último inicio de sesión del usuario.
++ **IsSuperuser**
+    <br> Determina si el usuario es un superuser. El valor por defecto es `cero` (`0`).
++ **Username**
+    <br> Nombre de usuario. Usado para el inicio de sesión.
++ **FirstName**
+    <br> Nombres. El valor por defecto es una `cadena vacía` (`''`).
++ **LastName**
+    <br> Apellidos. El valor por defecto es una `cadena vacía` (`''`).
++ **Email**
+    <br> Correo electrónico. El valor por defecto es una `cadena vacía` (`''`).
++ **IsStaff**
+    <br> Determina si el usuario pertenece al staff. Valor por defecto `cero` (`0`).
++ **IsActive**
+    <br> Indicador de habilitación. Determina si el usuario está habilitado o deshabilitado. Valor por defecto `uno` (`1`).
++ **DateJoined**
+    <br> Fecha y hora cuando el usuario fue registrado en el sistema.
++ **UpdatedAt**
+    <br> Fecha y hora cuando la información del usuario fue actualizada por última vez.
+
+### 🔸 SALES (Salts)
+Las **sales** (*salt*) son cademns usadas en la encriptación de contraseñas de los usuarios.
+
+![auth.Users](img/auth.Salts.png)
+
+```sql
+CREATE TABLE [auth].[Salts]
+(
+	  [Id]			[BIGINT] IDENTITY(1,1)	NOT NULL
+	, [UserId]		[CHAR](24)				NOT NULL
+	, [Salt]		[NVARCHAR](255)			NOT NULL
+	, [CreatedAt]	[DATETIMEOFFSET]		NULL
+	, [UpdatedAt]	[DATETIMEOFFSET]		NULL
+	, [HostName]	[NVARCHAR](128)			NULL
+	, [AppName]		[NVARCHAR](128)			NULL
+	, [UserName]	[NVARCHAR](128)			NULL
+)
+```
+
+### Referencia:
++ 🔑 **Id**
+    <br> Identificador único autogenerado de tipo entero (_BIGINT_ | _INT64_).
+    <br>
++ 🔗 **UserId**
+    <br> Identificador de usuario (_User.Id_)
+    <br> _ref._: [User](#users-table) 🌐
++ **Salt**
+    <br> Cadena usada en la encriptación.
++ **CreatedAt**
+    <br> Fecha y hora de inserción.
++ **UpdatedAt**
+    <br> Fecha y hora de la última actualización realizada.
+
+###### ・ Auditoría ・
+
++ **HostName**
+    <br> Estación de trabajo desde donde se realizó la actualización.
++ **AppName**
+    <br> Nombre del aplicativo desde donde se realizó la actualización.
++ **UserName**
+    <br> Nombre del usuario que realizó la actualización.
+
---
+++ src/SAXSAY/Structure/auth/Tables/Users.sql
@@ -0,0 +1,156 @@
+﻿/*
+<documentation>
+	<object type="U" schema="auth" name="Users" />
+	<summary>Users Table</summary>
+	<author>Jheison J. Mayta C.</author>
+	<createdAt>2024.10.27</createdAt>
+	<sourceLink></sourceLink>
+</documentation>
+*/
+CREATE TABLE [auth].[Users]
+(
+	  [Id]				[CHAR](24)			NOT NULL
+	, [Password]		[NVARCHAR](255)		NOT NULL
+	, [LastLogin]		[DATETIMEOFFSET]	NULL
+	, [IsSuperuser]		[BIT]				NULL
+	, [Username]		[VARCHAR](150)		NOT NULL
+	, [FirstName]		[NVARCHAR](250)		NULL
+	, [LastName]		[NVARCHAR](250)		NULL
+	, [Email]			[NVARCHAR](250)		NULL
+	, [IsStaff]			[BIT]				NULL
+	, [IsActive]		[BIT]				NOT NULL
+	, [DateJoined]		[DATETIMEOFFSET]	NULL
+	, [UpdatedAt]		[DATETIMEOFFSET]	NULL
+) ON [PRIMARY]
+GO
+
+-- CONSTRAINTS
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT PK_Users_Id
+		PRIMARY KEY CLUSTERED ([Id]);
+GO
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT DF_Users_IsSuperuser
+		DEFAULT 0 FOR [IsSuperuser];
+GO
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT DF_Users_FirstName
+		DEFAULT '' FOR [FirstName];
+GO
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT DF_Users_LastName
+		DEFAULT '' FOR [LastName];
+GO
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT DF_Users_Email
+		DEFAULT '' FOR [Email];
+GO
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT DF_Users_IsStaff
+		DEFAULT 0 FOR [IsStaff];
+GO
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT DF_Users_IsActive
+		DEFAULT 1 FOR [IsActive];
+GO
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT DF_Users_DateJoined
+		DEFAULT SWITCHOFFSET(SYSDATETIMEOFFSET(), '+00:00')
+			FOR [DateJoined];
+GO
+ALTER TABLE [auth].[Users]
+	ADD CONSTRAINT DF_Users_UpdatedAt
+		DEFAULT SWITCHOFFSET(SYSDATETIMEOFFSET(), '+00:00')
+			FOR [UpdatedAt];
+GO
+
+-- INDEXES
+CREATE NONCLUSTERED INDEX IX_Users_Username
+	ON [auth].[Users] ([Username])
+		INCLUDE ([FirstName], [LastName], [Email], [IsActive], [DateJoined])
+GO
+
+-- EXTENDED PROPERTIES
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Users Table'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'User Password'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'Password';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Date and time of the user''s last login.'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'LastLogin';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Determines if the user is a superuser. Default value is 0 (false)'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'IsSuperuser';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Username'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'Username';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'First name. Default value is empty'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'FirstName';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Last name. Default value is empty'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'LastName';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Email. Default value is empty'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'Email';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Determines if the user belongs to the staff. Default value is 0 (false)'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'IsStaff';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Enable indicator. Determines if the user is enabled or disabled.'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'IsActive';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Date and time when the user was registered in the system.'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'DateJoined';
+GO
+EXEC sp_addextendedproperty
+  @name			= N'MS_Description'
+, @value		= 'Date and time when the user''s information was updated.'
+, @level0type	= 'SCHEMA'		, @level0name	= N'auth'
+, @level1type	= 'TABLE'		, @level1name	= N'Users'
+, @level2type	= 'COLUMN'		, @level2name	= N'UpdatedAt';
+GO
