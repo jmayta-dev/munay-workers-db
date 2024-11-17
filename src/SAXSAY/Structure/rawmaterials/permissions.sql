92c464737856842bf6461c876f40bb6e2147987b
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-11-17 07:09:59 -0500

    add new permissions for client

--- src/SAXSAY/Structure/rawmaterials/permissions.sql
+++ src/SAXSAY/Structure/rawmaterials/permissions.sql
@@ -1,9 +1,11 @@
 
-GRANT EXECUTE
-	ON OBJECT::[rawmaterials].[usp_GetAllRawMaterials]
-	TO mwsaxsay;
+GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_GetAllRawMaterials] TO mwsaxsay;
 GO
-GRANT EXECUTE
-	ON OBJECT::[rawmaterials].[usp_GetRawMaterialsByFilter]
-	TO mwsaxsay;
+GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_GetRawMaterialsByFilter] TO mwsaxsay;
+GO
+GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_InsertRawMaterial] TO mwsaxsay;
+GO
+GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_RemoveRawMaterial] TO mwsaxsay;
+GO
+GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_UpdateRawMaterial] TO mwsaxsay;
 GO
