adfa24ea232841bba0f5f3aec7b94eba68c8fa8f
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2024-11-30 04:10:09 -0500

    add seed rawMaterials table

---
+++ src/SAXSAY/Scripts/Seed.sql.sql
@@ -0,0 +1,14 @@
+INSERT INTO rawmaterials.RawMaterials (
+	 Id
+	,Name
+	,UNSPSC
+	,UNSPSCDescription
+	,CreatedAt
+	,UpdatedAt
+	,IsEnabled
+) VALUES
+ ('001724050712673-WCDgTVUu'	,'pollo'		,'50111515000'	,'pollo, mínimamente procesado sin aditivos'	,GETDATE()	,GETDATE()	,1)
+,('001724052076233-0iio8LsB'	,'carne'		,'50111513000'	,'carne, mínimamente procesada sin aditivos'	,GETDATE()	,GETDATE()	,1)
+,('001724052134844-uOQUGKFN'	,'huevo'		,'50131612000'	,'huevos en la cascara de gallina'				,GETDATE()	,GETDATE()	,1)
+,('001724123587472-1DZCP5q5'	,'arroz'		,'10151701000'	,'semillas o plántulas de arroz'				,GETDATE()	,GETDATE()	,1)
+,('001724123934724-1tu8b1HF'	,'azúcar rubia'	,'50161814001'	,'azúcar o sustituto de azúcar, confite'		,GETDATE()	,GETDATE()	,1)
