99bca0478165997393c6cf4390b0c01f7da52734
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2025-03-01 22:58:54 -0500

    🛂 adds excecution permission
    - add execution permission on `usp_GetRawMaterialById`

--- src/SAXSAY/Structure/rawmaterials/permissions.sql
+++ src/SAXSAY/Structure/rawmaterials/permissions.sql
@@ -3,6 +3,8 @@ GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_GetAllRawMaterials] TO mwsaxsay;
 GO
 GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_GetRawMaterialsByFilter] TO mwsaxsay;
 GO
+GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_GetRawMaterialById] TO mwsaxsay;
+GO
 GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_InsertRawMaterial] TO mwsaxsay;
 GO
 GRANT EXECUTE ON OBJECT::[rawmaterials].[usp_RemoveRawMaterial] TO mwsaxsay;
