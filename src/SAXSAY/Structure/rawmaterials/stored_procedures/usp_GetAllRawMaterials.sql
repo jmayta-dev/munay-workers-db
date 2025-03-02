678668fac0266461783577285a26a3096115c632
Jheison J. Mayta C. <8401842+jmayta-dev@users.noreply.github.com>
2025-03-01 22:55:43 -0500

    ♻️ refactor to aligns script to convention

--- src/SAXSAY/Structure/rawmaterials/stored_procedures/usp_GetAllRawMaterials.sql
+++ src/SAXSAY/Structure/rawmaterials/stored_procedures/usp_GetAllRawMaterials.sql
@@ -1,24 +1,46 @@
+IF (OBJECT_ID(N'rawmaterials.usp_GetAllRawMaterials', N'P') IS NULL)
+EXECUTE('CREATE PROCEDURE rawmaterials.usp_GetAllRawMaterials AS SELECT 1')
+GO
+
+
+ALTER PROCEDURE rawmaterials.usp_GetAllRawMaterials (
+      @pOnlyEnabled bit = 0
+    , @pDebug       bit = 0
+)
 /*
 <documentation>
-	<object type="P" schema="rawmaterials" name="usp_GetAllRawMaterials" />
-	<summary></summary>
-	<author>Jheison J. Mayta C.</author>
-	<createdAt>2024.10.31</createdAt>
-	<sourceLink></sourceLink>
-	<example>
-		EXECUTE rawmaterials.usp_GetAllRawMaterials;
-	</example>
+    <object type="P" schema="rawmaterials" name="usp_GetAllRawMaterials" />
+    <summary>Retrieve all Raw Materials</summary>
+    <author>Jheison J. Mayta C.</author>
+    <createdAt>2024.10.31</createdAt>
+    <sourceLink></sourceLink>
+    <example>
+        EXECUTE rawmaterials.usp_GetAllRawMaterials;
+    </example>
 </documentation>
 */
-CREATE PROCEDURE [rawmaterials].[usp_GetAllRawMaterials]
 AS
 SET NOCOUNT ON;
 BEGIN
-	SELECT 
-		  [Id]			, [Name]
-		, [UNSPSC]		, [UNSPSCDescription]
-		, [CreatedAt]	, [UpdatedAt]
-		, [IsEnabled]
-	FROM [rawmaterials].[RawMaterials];
+    DECLARE
+          @vOnlyEnabled bit = COALESCE(@pOnlyEnabled, 0)
+        , @vDebug       bit = COALESCE(@pDebug, 0);
+
+    IF(@vDebug = 1)
+        PRINT CONCAT_WS('@vOnlyEnabled', '=', @vDebug);
+
+    IF (@vOnlyEnabled = 1)
+    BEGIN
+        SELECT
+            Id, Name, UNSPSC, CreatedAt, UpdatedAt, IsEnabled
+        FROM rawmaterials.RawMaterials
+        WHERE IsEnabled = 1;
+    END
+    ELSE
+    BEGIN
+        SELECT
+            Id, Name, UNSPSC, CreatedAt, UpdatedAt, IsEnabled
+        FROM rawmaterials.RawMaterials;
+    END
 END
 GO
